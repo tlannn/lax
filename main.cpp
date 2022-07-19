1deb740ccfb667ea6c83e5fdb28beaba7088627c
Theo Lannoy <theo.lannoy2.etu@univ-lille.fr>
2022-07-19 09:17:20 +0200

    fix: display only the output of the lax program
    Source code preview and output separator have been removed.

--- main.cpp
+++ main.cpp
@@ -3,55 +3,17 @@
 
 #include "lax.h"
 
-using namespace std;
-
 #define NO_ARGUMENT_CODE 1
-
-/**
- * Read a file
- *
- * @param filename the name of the file to read
- * @return the file content
- */
-string readFile(string filename);
 
 /// Main
 int main(int argc, char *argv[]) {
     if (argc == 1) {
-        printf("Please specify a file to run\n");
+        std::cout << "Please specify a file to run" << std::endl;
         exit(NO_ARGUMENT_CODE);
     }
 
-    string filepath = argv[1];
-    string source = readFile(filepath);
-
-    // Output source code
-    cout << source << endl;
-    cout << "------------------" << endl;
-    cout << " START OF PROGRAM" << endl;
-    cout << "------------------" << endl;
-
+    std::string filepath = argv[1];
 	Lax::run(filepath);
 
     return 0;
-}
-
-/// Read a file
-string readFile(string filename) {
-    string source = "";
-    string line;
-    ifstream file;
-    file.open(filename);
-
-    if (file.is_open()) {
-        while (getline(file, line))
-            source += line + '\n';
-
-        file.close();
-    }
-
-    else
-        cout << "Unable to open file";
-
-    return source;
 }
